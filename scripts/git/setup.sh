25d805580161025cc480102d560a237aef9e9a82
ionutbortis <ionutbortis@gmail.com>
2022-11-22 18:34:36 +0200

    Added check for existing profile

--- scripts/git/setup.sh
+++ scripts/git/setup.sh
@@ -118,14 +118,16 @@ display_profiles() {
   submodule_profile_check
 }
 
+existing_profile_error() {
+  local name="$1"
+  echo -e "\n[ ERROR ] A profile with name [ $name ] already exists!\n"
+}
+
 profile_name_error() {
   local name="$1"
-  echo
-  echo "[ ERROR ] The provided name [ $name ] is invalid!"
-  echo
-  echo "Please see the official git docs on how to name references (branches):"
-  echo "https://git-scm.com/docs/git-check-ref-format"
-  echo
+  echo -e "\n[ ERROR ] The provided name [ $name ] is invalid!"
+  echo -e "\nPlease see the official git docs on how to name references (branches):"
+  echo -e "https://git-scm.com/docs/git-check-ref-format\n"
 }
 
 create_new_profile() {
@@ -135,11 +137,13 @@ create_new_profile() {
   while [[ ! "$valid_name" ]]; do
     read -rp "Enter the new $PRJ_DISPLAY profile name: " name
 
-    if git check-ref-format --branch "$name" &>/dev/null; then
-      local valid_name="$name"
-    else
-      profile_name_error "$name"
-    fi
+    [[ " ${PROFILES_ARRAY[*]} " =~ " $name " ]] \
+        && { existing_profile_error "$name"; continue; }
+
+    git check-ref-format --branch "$name" &>/dev/null \
+        || { profile_name_error "$name"; continue; }
+
+    local valid_name="$name"
   done
 
   create_branch "$valid_name"
